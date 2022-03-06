93ee2f7c4a046bf6e993510f80e902f759909733
mingun0112 <mingun0112@naver.com>
2022-03-06 18:39:30 +0900

    3rd numpy test

--- 2nd/2nd.py
+++ 2nd/2nd.py
@@ -2,3 +2,4 @@ import numpy as np
 
 test_array = np.array(["1","4",5,8],float)
 print(test_array)
+
---
+++ 3rd/__pycache__/numpy_test.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+test_array=np.array(["1","4",5,8],float)
+print(test_array)
+
+print(type(test_array[3]))#numpy는 하나의 datatype만 취급=Dynamic typing 지원 X
+
+print(test_array.shape)
+
+
