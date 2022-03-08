27a322b05026675ac3fd639fe070e52b15331175
mingun0112 <mingun0112@naver.com>
2022-03-08 16:04:07 +0900

    riverTeam06_1st_mission

--- 1주차/riverTeam06_mission_1st.py
+++ 1주차/riverTeam06_mission_1st.py
@@ -26,3 +26,28 @@ def get_avg(score):
 
 get_avg(score)
 
+import copy
+dict_first={'사과': 30, '배': 15, '감': 10, '포도': 10}
+dict_second={'사과': 5, '감': 25, '배': 15, '귤': 25}
+
+def merge_dict(dict_first,dict_second):
+    temp=copy.deepcopy(dict_first)
+    temp.update(dict_second)
+    for i in dict_first:
+        if i in dict_second:
+            temp[i]=dict_first[i]+dict_second[i]
+    print(temp)
+
+merge_dict(dict_first,dict_second)
+
+import re
+
+def find_string(inputs):
+    ans_list = []
+    temp_str=re.sub(r"[0-9]+", " ", inputs)
+    ans_list=temp_str.split()
+    return ans_list
+inputs="cat32dog16cow5"
+string_list=find_string(inputs)
+print(string_list)
+
